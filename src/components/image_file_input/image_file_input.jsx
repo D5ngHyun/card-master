7f02a6d361c694c6505888aecf5a67044a01dbd4
DongHyun Lee <dmadkr1541@naver.com>
2022-05-17 09:50:44 +0900

    loading spinner

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import styles from './image_file_input.module.css'
 
 // const onFileChange = file => {
@@ -9,14 +9,16 @@ import styles from './image_file_input.module.css'
 //   })
 // }
 function ImageFileInput({ imageUploader, name, onFileChange }) {
+  const [ loading, setLoading ] = useState(false);
   const inputRef = useRef();
   const onButtonClick = (event) => {
     event.preventDefault();
     inputRef.current.click();
   }
   const onChange = async (event) => {
+    setLoading(true);
     const uploaded = await imageUploader.upload(event.target.files[0]);
-
+    setLoading(false);
     console.log(uploaded);
     
     // 함수에다가 파라미터로 name과 url을 보냄.
@@ -24,14 +26,23 @@ function ImageFileInput({ imageUploader, name, onFileChange }) {
       name: uploaded.original_filename,
       url: uploaded.url,
     })
-    
   }
 
   return <div className={styles.container}>
     <input className={styles.input} type="file" accept="image/*" name="file" ref={inputRef} onChange={onChange} />
-    <button className={styles.button} onClick={onButtonClick}>
-      {name || 'No file'}
-    </button>
+    {
+      !loading && (
+        <button className={`${styles.button} ${name ? styles.pink : styles.grey}`} onClick={onButtonClick}>
+          {name || 'No file'}
+        </button>
+      )
+    }
+    
+
+    {
+      loading && <div className={styles.loading}></div>
+    }
+    
   </div>
 }
 
